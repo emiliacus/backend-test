3c6a5ad5d19b644fb602940195a08aa4516dc1c9
Emilio Andujar <emilio.andujar@netberry.es>
2021-05-30 13:47:41 +0200

    Refactor Item.php in level 6 (PHPStan)

--- src/Item.php
+++ src/Item.php
@@ -3,17 +3,45 @@
 namespace Runroom\GildedRose;
 
 class Item {
-
+    /**
+    * string name var
+    *
+    * @var string
+    */
     public $name;
+
+    /**
+    * int sell_in var
+    *
+    * @var int
+    */
     public $sell_in;
+
+    /**
+    * int quality var
+    *
+    * @var int
+    */
     public $quality;
 
-    function __construct($name, $sell_in, $quality) {
+    /**
+    * Item constructor
+    *
+    * @param string $name
+    * @param int $sell_in
+    * @param int $quality
+    */
+    function __construct( $name, $sell_in, $quality ) {
         $this->name = $name;
         $this->sell_in = $sell_in;
         $this->quality = $quality;
     }
 
+    /**
+    * Function to convert Item to string
+    *
+    * @return string
+    */
     public function __toString() {
         return "{$this->name}, {$this->sell_in}, {$this->quality}";
     }
