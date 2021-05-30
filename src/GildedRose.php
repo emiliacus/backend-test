3c5e45afca7664cafe68969d3b3f2102de562249
Emilio Andujar <emilio.andujar@netberry.es>
2021-05-30 13:38:19 +0200

    Refactor GlidedRose.php in level 6 (PHPStan)

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -3,32 +3,47 @@
 namespace Runroom\GildedRose;
 
 class GildedRose {
-
+    /**
+    * Array of Item objects var
+    *
+    * @var Item[]
+    */
     private $items;
 
-    function __construct($items) {
+    /**
+    * GildeRose constructor
+    *
+    * @param Item[] $items
+    */
+    function __construct( $items ) {
         $this->items = $items;
     }
 
+    /**
+    * Function to update quality parameter of Item objects.
+    *
+    * @return void
+    */
     function update_quality() {
-        foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+        /* @var $items Item[] */
+        foreach ( $this->items as $item ) {
+            if ( $item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert' ) {
+                if ( $item->quality > 0 ) {
+                    if ( $item->name != 'Sulfuras, Hand of Ragnaros' ) {
                         $item->quality = $item->quality - 1;
                     }
                 }
             } else {
-                if ($item->quality < 50) {
+                if ( $item->quality < 50 ) {
                     $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sell_in < 11) {
-                            if ($item->quality < 50) {
+                    if ( $item->name == 'Backstage passes to a TAFKAL80ETC concert' ) {
+                        if ( $item->sell_in < 11 ) {
+                            if ( $item->quality < 50 ) {
                                 $item->quality = $item->quality + 1;
                             }
                         }
-                        if ($item->sell_in < 6) {
-                            if ($item->quality < 50) {
+                        if ( $item->sell_in < 6 ) {
+                            if ( $item->quality < 50 ) {
                                 $item->quality = $item->quality + 1;
                             }
                         }
@@ -36,15 +51,15 @@ class GildedRose {
                 }
             }
 
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+            if ( $item->name != 'Sulfuras, Hand of Ragnaros' ) {
                 $item->sell_in = $item->sell_in - 1;
             }
 
-            if ($item->sell_in < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
+            if ( $item->sell_in < 0 ) {
+                if ( $item->name != 'Aged Brie' ) {
+                    if ( $item->name != 'Backstage passes to a TAFKAL80ETC concert' ) {
+                        if ( $item->quality > 0 ) {
+                            if ( $item->name != 'Sulfuras, Hand of Ragnaros' ) {
                                 $item->quality = $item->quality - 1;
                             }
                         }
@@ -52,7 +67,7 @@ class GildedRose {
                         $item->quality = $item->quality - $item->quality;
                     }
                 } else {
-                    if ($item->quality < 50) {
+                    if ( $item->quality < 50 ) {
                         $item->quality = $item->quality + 1;
                     }
                 }
